fcb881c5cc122569dce220b670232848512f6cdb
Chekmaeva <45551848+Chekmaeva@users.noreply.github.com>
2019-03-31 11:47:14 +0300

    Задача "Новости и пользователи"

---
+++ Новости и пользователи.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+from requests import get
+import JSON
+from flask import Flask, render_template, make_response, request
+from flask_wtf import FlaskForm
+ 
+app = Flask(__name__)
+@app.route('/')
+def pereadress():
+    return render_template('Переадресация.html',
+                           title='Возврат на предыдущую страницу')
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/users',  methods=['GET'])
+def get_news():
+    news = NewsModel(db.get_connection()).get_all()
+    return jsonify({'news': news})
+
+@app.route('/users/<int:news_id>',  methods=['GET'])
+def get_one_news(news_id):
+    news = NewsModel(db.get_connection()).get(news_id)
+    if not news:
+        return jsonify({'error': 'Not found'})
+    return jsonify({'news': news})
+
+@app.route('/users', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in ['title', 'content', 'user_id']):
+        return jsonify({'error': 'Bad request'})
+    news = NewsModel(db.get_connection())
+    news.insert(request.json['title'], request.json['content'],
+                request.json['user_id'])
+    return jsonify({'success': 'OK'})
+
+@app.route('/users/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    news = NewsModel(db.get_connection())
+    if not news.get(news_id):
+        return jsonify({'error': 'Not found'})
+    news.delete(news_id)
+    return jsonify({'success': 'OK'})
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
